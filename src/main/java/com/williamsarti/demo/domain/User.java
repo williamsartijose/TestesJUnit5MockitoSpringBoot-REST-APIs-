aad70168da2586ac50978465b6cc5c999e5154db
unknown <williamsartijose@hotmail.com>
2023-02-22 19:51:54 +0000

    6. Aula 03 - Classe de domínio - Criando a classe Domain-User

---
+++ src/main/java/com/williamsarti/demo/domain/User.java
@@ -0,0 +1,27 @@
+package com.williamsarti.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	private String password;
+
+}
