f9f2a0dbf8d2310f11206a2e0a859d6f0576a697
unknown <williamsartijose@hotmail.com>
2023-04-04 11:23:39 +0100

    Endpoint delete para USER

--- src/main/java/com/williamsarti/demo/resources/UserResource.java
+++ src/main/java/com/williamsarti/demo/resources/UserResource.java
@@ -50,6 +50,10 @@ public class UserResource {
         return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
 
     }
+@DeleteMapping( value = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
 
-    
+    }
 }
--- src/main/java/com/williamsarti/demo/services/UserService.java
+++ src/main/java/com/williamsarti/demo/services/UserService.java
@@ -12,5 +12,6 @@ public interface UserService {
 	 List<User> findAll();
 	 User create(UserDTO obj);
 	 User update(UserDTO obj);
+	void delete(Integer id);
 
 }
