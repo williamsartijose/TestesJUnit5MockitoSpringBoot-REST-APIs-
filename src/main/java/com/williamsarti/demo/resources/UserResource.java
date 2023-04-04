f64caf74553c28d0ca73cd67416ede736d55c506
unknown <williamsartijose@hotmail.com>
2023-04-04 11:11:01 +0100

    Execacao para email ja cadastrado  Update

--- src/main/java/com/williamsarti/demo/resources/UserResource.java
+++ src/main/java/com/williamsarti/demo/resources/UserResource.java
@@ -4,15 +4,11 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.williamsarti.demo.domain.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.williamsarti.demo.domain.dto.UserDTO;
@@ -47,5 +43,13 @@ public class UserResource {
                 (service.create(obj).getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @PutMapping (value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
+        obj.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
+
+    }
+
     
 }
